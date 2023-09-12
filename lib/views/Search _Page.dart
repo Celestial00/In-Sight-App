3e5bf577e59389a0b57635fa68b668d651d1dee9
celestial00 <sheerazalee223@gmail.com>
2023-09-12 15:17:56 +0500

    kasam sa final commit

--- lib/views/Search _Page.dart
+++ lib/views/Search _Page.dart
@@ -45,13 +45,24 @@ class _SearchPageState extends State<SearchPage> {
                 onChanged: (value) {},
               ),
             ),
-            const Padding(
+            Padding(
               padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              child: Row(
-                children: [
-                  Text("Result's"),
-                ],
-              ),
+              child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                Text("Results",
+                    style: TextStyle(
+                        fontFamily: "Nunito",
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: Sec_Color)),
+                SizedBox(
+                  width: 29,
+                ),
+                Container(
+                  width: 350,
+                  height: 0.8,
+                  decoration: BoxDecoration(color: Colors.grey.shade500),
+                )
+              ]),
             ),
             const BookMarkCard(),
             const BookMarkCard(),
